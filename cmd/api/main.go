45b1a57785bfb32d8392b04963602c0ad80082e5
Burhaan Ashraf <burhaanashraf@outlook.com>
2026-09-06 17:51:40 +0530

    bootstrap API application startup

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"time"
+
+	"github.com/BurhaanAshraf/job-scheduler-platform/internal/config"
+	"github.com/BurhaanAshraf/job-scheduler-platform/internal/db"
+	"github.com/BurhaanAshraf/job-scheduler-platform/internal/logger"
+)
 
 func main() {
-	fmt.Println("API server placeholder...")
+
+	log := logger.New("api")
+
+	cfg, err := config.Load()
+	if err != nil {
+		log.Error("something wrong with config", "err", err)
+		return
+	}
+
+	startupCtx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	pool, err := db.NewPool(startupCtx, cfg)
+	if err != nil {
+		log.Error("error creating connection pool", "err", err)
+		return
+	}
+
+	defer pool.Close()
+
+	log.Info("API server is not implemented yet...")
+
 }
